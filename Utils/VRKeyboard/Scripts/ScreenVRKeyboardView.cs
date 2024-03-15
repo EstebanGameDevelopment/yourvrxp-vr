b68aba797bf7bdc08d3720efc6e82a5db41dd1c2
DESKTOP-K1COMOS\esteb <services@yourvrexperience.com>
2024-03-15 17:12:28 +0100

    Use voice speech recognition to enter text input.

--- Utils/VRKeyboard/Scripts/ScreenVRKeyboardView.cs
+++ Utils/VRKeyboard/Scripts/ScreenVRKeyboardView.cs
@@ -7,6 +7,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using yourvrexperience.Utils;
 using static TMPro.TMP_InputField;
+#if ENABLE_SPEECH
+using yourvrexperience.speech;
+#endif
 
 namespace yourvrexperience.VR
 {
@@ -17,10 +20,18 @@ namespace yourvrexperience.VR
         public const string EventScreenVRKeyboardSetNewText = "EventScreenVRKeyboardSetNewText";
         public const string EventScreenVRKeyboardConfirmInput = "EventScreenVRKeyboardConfirmInput";
 
+        [SerializeField] private GameObject contentKeyboard;
+        [SerializeField] private Button btnRecordVoice;
+        [SerializeField] private GameObject contentProcessing;
+        [SerializeField] private Button btnStopRecording;
+        [SerializeField] private TextMeshProUGUI textProcessing;
+
 		private GameObject _target;
         private KeyboardManager _keyboardManager;
         private TMP_InputField _inputField;
         private ContentType _typeContent = ContentType.Standard;
+
+        private float _timeToRecord = -1;
 
         public override void Initialize(params object[] parameters)
         {
@@ -49,7 +60,18 @@ namespace yourvrexperience.VR
             }
             _keyboardManager.maxInputLength = (int)parameters[2];
             _keyboardManager.Initialize(_typeContent);
+
             UIEventController.Instance.Event += OnUIEvent;
+            SystemEventController.Instance.Event += OnSystemEvent;
+
+#if ENABLE_SPEECH
+            btnRecordVoice.onClick.AddListener(OnRecordMessage);
+            btnStopRecording.onClick.AddListener(OnStopRecording);            
+            contentProcessing.gameObject.SetActive(false);
+#else
+            btnRecordVoice.gameObject.SetActive(false);
+            contentProcessing.gameObject.SetActive(false);
+#endif            
 		}
 
         void OnDestroy()
@@ -66,11 +88,64 @@ namespace yourvrexperience.VR
 
                 base.Destroy();
 
-                UIEventController.Instance.Event -= OnUIEvent;
+                if (UIEventController.Instance != null) UIEventController.Instance.Event -= OnUIEvent;
+                if (SystemEventController.Instance != null) SystemEventController.Instance.Event -= OnSystemEvent;
 
 				UIEventController.Instance.DispatchUIEvent(ScreenController.EventScreenControllerDestroyScreen, this.gameObject);
             }
         }
+
+#if ENABLE_SPEECH
+        private void OnStopRecording()
+        {
+            btnRecordVoice.gameObject.SetActive(false);
+            btnStopRecording.gameObject.SetActive(false);
+            contentProcessing.gameObject.SetActive(true);
+            _timeToRecord = -1;
+            textProcessing.text = LanguageController.Instance.GetText("vr.keyboard.now.processing");
+            SpeechRecognitionController.Instance.ProcessSpeech(this.gameObject);
+        }
+
+        private void OnRecordMessage()
+        {
+            contentKeyboard.SetActive(false);
+            btnRecordVoice.gameObject.SetActive(false);
+            contentProcessing.gameObject.SetActive(true);
+            _timeToRecord = 10;
+            textProcessing.text = LanguageController.Instance.GetText("vr.keyboard.speak.now") + (int)_timeToRecord + "...";
+            SpeechRecognitionController.Instance.StartRecording();
+        }
+#endif        
+
+#if ENABLE_SPEECH
+        private void OnSystemEvent(string nameEvent, object[] parameters)
+        {
+            if (nameEvent.Equals(SpeechRecognitionController.EventTextToSpeechControllerTimeoutProcessing))
+            {
+                btnRecordVoice.gameObject.SetActive(true);
+                contentProcessing.gameObject.SetActive(false);
+                contentKeyboard.SetActive(true);
+            }
+            if (nameEvent.Equals(SpeechRecognitionController.EventSpeechRecognitionControllerCompleted))
+            {
+                bool isOk = (bool)parameters[0];
+                string resultText = (string)parameters[1];
+                GameObject target = (GameObject)parameters[2];
+                btnRecordVoice.gameObject.SetActive(true);
+                contentProcessing.gameObject.SetActive(false);
+                contentKeyboard.SetActive(true);
+                _timeToRecord = -1;
+                SystemEventController.Instance.ClearSystemEvents(SpeechRecognitionController.EventTextToSpeechControllerTimeoutProcessing);
+                if (this.gameObject == target)
+                {
+                    if (isOk)
+                    {
+                        _keyboardManager.inputText.text = resultText;
+                    }
+                }
+            }
+        }
+#endif        
 
         private void OnUIEvent(string nameEvent, params object[] parameters)
 		{
@@ -82,5 +157,22 @@ namespace yourvrexperience.VR
                 UIEventController.Instance.DispatchUIEvent(EventScreenVRKeyboardSetNewText, _target, finalText);
             }
 		}
+
+        void Update()
+        {
+#if ENABLE_SPEECH
+            if (_timeToRecord > 0)
+            {
+                _timeToRecord -= Time.deltaTime;
+                textProcessing.text = LanguageController.Instance.GetText("vr.keyboard.speak.now") + (int)_timeToRecord + "...";
+                if (_timeToRecord < 0)
+                {
+                    _timeToRecord = -1;                    
+                    OnStopRecording();
+                }
+            }
+#endif
+        }
+
     }
 }
